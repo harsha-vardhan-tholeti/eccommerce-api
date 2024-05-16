965940b5aab4d03f436e6339db14e0c9fb8a3efd
Tholeti Harsha Vardhan <106816042+harsha-vardhan-tholeti@users.noreply.github.com>
2024-05-16 22:13:17 +0530

    register user api

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,3 +1,48 @@
-const register = () => {};
+import { hashPassword } from "../helpers/auth.helpers.js";
+import User from "../models/user.model.js";
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    if (!name) return res.send({ error: "Name is required" });
+    if (!email) return res.send({ error: "Email is required" });
+    if (!password) return res.send({ error: "Password is required" });
+    if (!phone) return res.send({ error: "Phone is required" });
+    if (!address) return res.send({ error: "Address is required" });
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already Register Please Login",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "User Register Successfully",
+      newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registration",
+      error,
+    });
+  }
+};
 
 export { register };
